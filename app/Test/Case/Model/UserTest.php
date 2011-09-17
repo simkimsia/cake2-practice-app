3d11757e245f25391a82bd1715efcde1d511dd0e
kimsia <kimcity@gmail.com>
2011-09-17 13:53:47 +0800

    added the User Test case.

---
+++ app/Test/Case/Model/UserTest.php
@@ -0,0 +1,40 @@
+<?php
+/* User Test cases generated on: 2011-09-17 04:06:17 : 1316232377*/
+App::uses('User', 'Model');
+
+/**
+ * User Test Case
+ *
+ */
+class UserTestCase extends CakeTestCase {
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array('app.user', 'app.group', 'app.instructor', 'app.course', 'app.student');
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->User = ClassRegistry::init('User');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->User);
+		ClassRegistry::flush();
+
+		parent::tearDown();
+	}
+
+}
